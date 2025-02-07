edd85723658ac34fe2abf29c6177ac5ea29a430f
BaghettiGH <trollolol846@gmail.com>
2025-02-07 15:28:12 +0800

    setting calc. page

--- cardinal-quantify/src/App.js
+++ cardinal-quantify/src/App.js
@@ -1,14 +1,26 @@
-
-import './App.css';
-import Sidebar from './components/Sidebar';
+import React, { useState } from "react";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+
   return (
     <div className="app">
-      <Sidebar />
-      <div className="main-content">
+      {/* Pass toggle function to Sidebar */}
+      <Sidebar toggleSidebar={() => setIsSidebarCollapsed(!isSidebarCollapsed)} />
+      
+      {/* Adjust main content based on sidebar state */}
+      <div className={`main-content ${isSidebarCollapsed ? "collapsed" : "expanded"}`}>
         <h1>Course Name</h1>
         {/* Other UI content here */}
+        <div className ="taskbar">
+          <ul>
+            <li>Compute Grade</li>
+            <li>Estimate Grade</li>
+          </ul>
+          
+          </div>
       </div>
     </div>
   );
--- cardinal-quantify/src/components/Sidebar.js
+++ cardinal-quantify/src/components/Sidebar.js
@@ -1,23 +1,26 @@
-import React from "react";
-import "./Sidebar.css";
+import React, { useState } from "react";
+import "./Sidebar.css"; 
+
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
+
+  return (
+    <div className="sidebar">
+    
+        <div>
             <div className="logo">
                 <h2>Cardinal Quantify</h2>
-                    {/* replace this with an image */}
             </div>
-            <div className ="profile">
-                {/* add profile picture placeholder */}
-                <p>User name</p>
-            </div>
-        <ul>
+          <div className="profile">
+            <p>User Name</p>
+          </div>
+          <ul>
             <li>Courses</li>
             <li>Feedback</li>
-            <li>Signout</li>
-        </ul>
+            <li>Sign Out</li>
+          </ul>
         </div>
-    );
+    </div>
+  );
 };
 
 export default Sidebar;
