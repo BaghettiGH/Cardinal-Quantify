7498726a6ca712836b44a74c038603bcf0772d57
Angela Sta. Maria <gelaysabelle@gmail.com>
2025-02-12 01:18:11 +0800

    fixed navbar (still needs fixing in the interface) but overall the functionality is good, added assets folder, page folder, and style folder for sass

---
+++ cardinal-quantify/src/components/Navbar.jsx
@@ -0,0 +1,87 @@
+import React,  { useState } from 'react';
+import Logo from '../Assets/Logo.svg';
+import { FaRegUser } from "react-icons/fa";
+import { FaBook } from "react-icons/fa";
+import { MdFeedback } from "react-icons/md";
+import { FaSignOutAlt } from "react-icons/fa";
+
+import { useLocation } from "react-router-dom";
+
+const Navbar = () => { 
+    const location = useLocation();
+
+    const [closeMenu, setCloseMenu] = useState(false);
+
+    const handleCloseMenu = () => {
+        setCloseMenu(!closeMenu);
+    };
+
+    return (
+        <div className={closeMenu === false ? "sidebar" : "sidebar active"}>
+            <div
+                className={
+                    closeMenu === false
+                        ? "logoContainer"
+                        : "logoContainer active"
+                }
+            >
+                <img src={Logo} alt="logo" className="logo" />
+                <h2 className="title">Quantify </h2>
+            </div>
+            <div
+                className={
+                    closeMenu === false
+                        ? "burgerContainer"
+                        : "burgerContainer active"
+                }
+            >
+                <div
+                    className="burgerTrigger"
+                    onClick={() => {
+                        handleCloseMenu();
+                    }}
+                ></div>
+                <div className="burgerMenu"></div>
+            </div>
+            <div
+                className={
+                    closeMenu === false
+                        ? "profileContainer"
+                        : "profileContainer active"
+                }
+            >
+                
+                <FaRegUser className="profile" size="2em" color="white"/>
+                <div className="profileContents">
+                    <p className="name"> HELLO WORLD </p>                    
+                </div>
+            </div>
+            <div
+                className={
+                    closeMenu === false
+                        ? "contentsContainer"
+                        : "contentsContainer active"
+                }
+            >
+                <ul>
+                    <li className={location.pathname === "/" ? "active" : ""}>
+                      <FaBook color="white" size = "1.5em" className="icons" />
+                        <a href="/">Courses</a>
+                    </li>
+                    <li className={location.pathname === "/feedback" ? "active" : ""}>
+                    <MdFeedback color="white" size = "1.5em" className="icons" />
+                        <a href="/feedback">Feedback</a>
+                    </li>
+
+                    <li className={location.pathname === "/signout" ? "active" : ""}>
+                    <FaSignOutAlt color="white" size = "1.5em" className="icons" />
+                        <a href="/signout">Signout</a>
+                    </li>
+                   
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- cardinal-quantify/src/components/Taskbar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-<div className = "gradetable">
-    <ul>
-        <li>Compute Grade</li>
-        <li>Estimate Grade</li>
-    </ul>    
-</div>
