cb4eb398a823477b2b9da6f58680669fc0f47366
Angela Sta. Maria <gelaysabelle@gmail.com>
2025-02-22 22:21:15 +0800

    Added login and signup, connected to firebase

--- cardinal-quantify/src/App.jsx
+++ cardinal-quantify/src/App.jsx
@@ -1,30 +1,66 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "../src/styles/main.scss";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import Courses from "./pages/Courses";
 import Feedback from "./pages/Feedback";
 
-import Signout from "./pages/Signout";
+
 import Sidebar from "./components/Navbar";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { auth } from "./components/firebase";
+import { Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 
 function App() {
+
+ 
+  const [user, setUser] = useState();
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  });
+
   
   return (
-    <div>
-      
+   
       <Router>
-     
-      
-      <Routes>
-        <Route path = "/" element={<Login />} />
-        <Route path = "/signup" element={<Signup />} />
-      </Routes>
-     
+        <div className="App">
+          <div className="auth-wrapper">
+            <div className="auth-inner">
+              
+              <Routes>
+              <Route
+                path="/"
+                element={user ? <Navigate to="/dashboard" /> : <Login />}
+              />
+                <Route path = "/login" element={<Login />} />
+                <Route path = "/signup" element={<Signup />} />
+
+                <Route 
+                element = {
+                  <>
+                   <Navbar />
+                   <Outlet />
+                  </>
+                }>
+
+                <Route path = "/dashboard" element={<Sidebar />} />
+                <Route path="/courses" element={<Courses />} />
+                <Route path="/feedback" element={<Feedback />} />
+                </Route>
+              </Routes>
+              <ToastContainer />
+            </div>
+          </div>
+        </div>
       </Router>
-    </div>
+
   );
 }
 
--- cardinal-quantify/src/pages/Signout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Signout = () => {
-  return (
-    <div><h1>Signout</h1></div>
-  )
-}
-
-export default Signout;
