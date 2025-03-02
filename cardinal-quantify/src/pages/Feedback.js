4e727b96c1d7c0d3495b9b5d56bfa01c6de44eac
BaghettiGH <trollolol846@gmail.com>
2025-03-02 22:20:55 +0800

    feedback page confimation almost ok

--- cardinal-quantify/src/pages/Feedback.js
+++ cardinal-quantify/src/pages/Feedback.js
@@ -1,14 +1,17 @@
-import React, { useState, useEffect, use } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaArrowRight } from "react-icons/fa";
 import { collection, addDoc } from "firebase/firestore";
 import { auth, db } from "../components/firebase";
 import { doc, getDoc } from "firebase/firestore";
+import { FaCheckCircle } from "react-icons/fa";
 import "../styles/feedback.scss";
+import { IoMdClose } from "react-icons/io";
 
 const Feedback = () => {
   const [userDetail, setUserDetail] = useState('Anonymous');
   const [message, setMessage] = useState('');
   const [submitted, setSubmitted] = useState(false);
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
   const fetchUserData = async () => {
     auth.onAuthStateChanged(async (user) => {
@@ -26,17 +29,6 @@ const Feedback = () => {
       useEffect(() => {
         fetchUserData();
       }, []);
-
-
-  // useEffect(() => {
-  //   const unsubscribe = auth.onAuthStateChanged((user) => {
-  //     if (user) {
-  //       setUserName(user.displayName || "Anonymous");
-  //     }
-  //   });
-
-  //   return () => unsubscribe();
-  // }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -56,6 +48,7 @@ const Feedback = () => {
       // console.log("Feedback submitted:", { userDetail.firstName, message });
       setSubmitted(true);
       setMessage(""); 
+      setShowConfirmation(true);
     } catch (error) {
       console.error("Error submitting feedback:", error);
     }
@@ -72,9 +65,27 @@ const Feedback = () => {
         onChange={(e) => setMessage(e.target.value)}
         />
         </div>
-        <button className = "feedback-submit" onClick = {handleSubmit} >Submit <FaArrowRight /></button>
-
+        <button className = "feedback-submit" 
+        onClick = {handleSubmit} >
+          Submit <FaArrowRight />
+        </button>
       </div>
+      {showConfirmation && (
+        <div className = {"overlay show" }>
+          <div className = "overlay-content">
+          <button className = "close-button"
+        onClick = {() => setShowConfirmation(false)}>
+          <IoMdClose />
+        </button>
+          <div className = "check-icon-container">
+          <FaCheckCircle className = "check-icon"/>
+          </div>
+        <h2>Thank you!</h2>
+        <h4>We appreciate your input and are committed to improving our services based on your suggestions.</h4>
+
+        </div>
+        </div>
+      )}
     </div>
   );
 };
